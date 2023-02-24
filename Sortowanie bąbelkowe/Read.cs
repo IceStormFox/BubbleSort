cc2e4fa044f0587397cab41238abca3ba772f779
IceStormFox <icestormfox@yahoo.com>
2023-02-24 01:15:34 +0100

    Update

---
+++ Sortowanie bąbelkowe/Read.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace BubbleSort
+{
+    public class Read
+    {
+        public static void Reader(string path)
+        {
+            string czytnik;
+            int indeks = 0;
+            int[] array = new int[200000];
+            Stopwatch timer = new Stopwatch();
+            StreamReader dane;
+
+            dane = new StreamReader(path);
+            while ((czytnik = dane.ReadLine()) != null)
+            {
+                array[indeks] = int.Parse(czytnik);
+                indeks++;
+            }
+
+            timer.Start();
+            Sort.Sortuj(array);
+            timer.Stop();
+
+            Console.WriteLine("Czas wykonania sortowania: {0} ms", timer.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Sortowanie bąbelkowe/Sort.cs
@@ -0,0 +1,24 @@
+﻿namespace BubbleSort
+{
+    public class Sort
+    {
+        public static void Sortuj(int[] sortowanie)
+        {
+            int n = sortowanie.Length;
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    for (int j = 0; j < n - 1 - i; j++)
+                    {
+                        if (sortowanie[j] > sortowanie[j + 1])
+                        {
+                            int zmienna = sortowanie[j];
+                            sortowanie[j] = sortowanie[j + 1];
+                            sortowanie[j + 1] = zmienna;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
